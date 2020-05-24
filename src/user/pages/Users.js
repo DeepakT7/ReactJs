4d95fd2b69e6c701279cb9aa32b05b7a497a179b
Deepak Tejwani <deepaktejwani1@gmail.com>
2020-05-24 19:39:36 +0530

    Create Users.js

---
+++ src/user/pages/Users.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+    const USERS = [{
+        id: 'u1',
+        name : 'Deepak Tejwani',
+        image: 'https://i.pinimg.com/originals/4c/52/f9/4c52f9eb7ff10b55e35ab24634db6a57.jpg',
+        places = 3
+
+
+    }];
+
+    return <UsersList items = {}/>;
+};
+
+export default Users;
