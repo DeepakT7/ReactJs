5f1fab05f117bbb6574ea8d37e157579cb77c2a0
Deepak Tejwani <deepaktejwani1@gmail.com>
2020-05-26 19:50:50 +0530

    Update PlaceList.js

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-
+import PlaceItem  from './PlaceItem';
 import Card from '../.../shared/components/UIElements/card';
 import './PlacesList.css';
 
 const PlacesList = props => {
     if (props.items.length === 0){
-        return <div className = "place-list center">
+        return (
+        <div className = "place-list center">
             <Card>
                 <h2>
                     No Places Found, Maybe Create one?
@@ -16,7 +17,24 @@ const PlacesList = props => {
                 </button>
             </Card>
         </div>
+        );
     }
+
+    return (
+        <ul className = "place-list">
+            {props.items.map(place => <PlaceItem 
+            key ={place.id}
+            image = {place.imageUrl}
+            title = {place.title}
+            description = {place.description}
+            address = {place.address}
+            creatorId = {place.creator}
+            coordinates = {place.location}
+            />
+            )
+            }
+        </ul>
+    );
 };
 
 export default PlacesList;
