451c77a9ef34fd76f51c8e498a06d70423982c88
Deepak Tejwani <deepaktejwani1@gmail.com>
2020-05-26 19:07:17 +0530

    Update Users.js

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -7,9 +7,7 @@ const Users = () => {
         id: 'u1',
         name : 'Deepak Tejwani',
         image: 'https://i.pinimg.com/originals/4c/52/f9/4c52f9eb7ff10b55e35ab24634db6a57.jpg',
-        places = 3
-
-
+        places : 3
     }];
 
     return <UsersList items = {}/>;
